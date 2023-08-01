db9e07e66b19d6d53380c501155ef505ac3a2048
anroy-learn <98684225+anroy-learn@users.noreply.github.com>
2023-08-01 17:25:55 -0400

    Initial Commit

---
+++ Partner practice/Partner_practiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  Partner_practiceApp.swift
+//  Partner practice
+//
+//  Created by Angel Roy on 8/1/23.
+//
+
+import SwiftUI
+
+@main
+struct Partner_practiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
